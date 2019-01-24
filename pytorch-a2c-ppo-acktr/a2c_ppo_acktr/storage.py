9e6b035e2c07e3cb78ad24cd93aadfbb3fd4f0e2
srikarym <msy290@nyu.edu>
2019-01-24 16:51:00 -0500

    10 examples

--- pytorch-a2c-ppo-acktr/a2c_ppo_acktr/storage.py
+++ pytorch-a2c-ppo-acktr/a2c_ppo_acktr/storage.py
@@ -2,31 +2,34 @@ import torch
 from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 import numpy as np
 
-def _flatten_helper(T, N, _tensor):
+# def _flatten_helper(T, N, _tensor):
+	# return _tensor.view(T * N, *_tensor.size()[2:])
+
+def _flatten_helper(_tensor):
 	return _tensor.view(T * N, *_tensor.size()[2:])
 
-
 class RolloutStorage(object):
-	def __init__(self, num_steps, num_processes, obs_shape, action_space):
+	def __init__(self, num_steps,num_rolls_per_sen, num_processes, obs_shape, action_space):
 		# self.obs = torch.zeros(num_steps + 1, num_processes, *obs_shape)
 		self.obs_s = []
 		self.obs_t = []
 		self.num_processes = num_processes
 		self.num_steps = num_steps
+		self.num_rolls_per_sen = num_rolls_per_sen
 		# self.recurrent_hidden_states = torch.zeros(num_steps + 1, num_processes, recurrent_hidden_state_size)
-		self.rewards = torch.zeros(num_steps*num_processes,1)
-		self.value_preds = torch.zeros(num_steps*num_processes+1,1)
+		self.rewards = torch.zeros(num_steps*num_rolls_per_sen,num_processes,1)
+		self.value_preds = torch.zeros((num_steps+1)*num_rolls_per_sen,num_processes,1)
 		# self.returns = torch.zeros(num_steps*num_processes+1,1)
-		self.returns = np.zeros((num_steps*num_processes+1,1))
-		self.action_log_probs = torch.zeros(num_steps*num_processes,1)
+		self.returns = np.zeros(((num_steps+1)*num_rolls_per_sen,num_processes,1))
+		self.action_log_probs = torch.zeros((num_steps)*num_rolls_per_sen,num_processes,1)
 		if action_space.__class__.__name__ == 'Discrete':
 			action_shape = 1
 		else:
 			action_shape = action_space.shape[0]
-		self.actions = torch.zeros(num_steps* num_processes, action_shape)
+		self.actions = torch.zeros(num_steps*num_rolls_per_sen, num_processes, action_shape)
 		if action_space.__class__.__name__ == 'Discrete':
 			self.actions = self.actions.long()
-		self.masks = torch.ones((num_steps + 1)* num_processes, 1)
+		self.masks = torch.ones((num_steps + 1)*num_rolls_per_sen, num_processes,1)
 		# self.rewards = torch.zeros(num_steps, num_processes, 1)
 		# self.value_preds = torch.zeros(num_steps + 1, num_processes, 1)
 		# self.returns = torch.zeros(num_steps + 1, num_processes, 1)
@@ -35,6 +38,7 @@ class RolloutStorage(object):
 
 		
 		self.step = 0
+		self.roll = 0
 
 	def to(self, device):
 		# self.obs = self.obs.to(device)
@@ -51,18 +55,27 @@ class RolloutStorage(object):
 		# self.obs_s = torch.cat(obs[0])
 		# self.obs_t = torch.cat(obs[1])
 		# self.recurrent_hidden_states[self.step + 1].copy_(recurrent_hidden_states)
+		self.actions[self.step*self.roll + self.roll].copy_(actions.squeeze())
+		self.action_log_probs[self.step*self.roll + self.roll].copy_(action_log_probs.squeeze())
+		self.value_preds[self.step*self.roll + self.roll].copy_(value_preds.squeeze())
+		self.rewards[self.step*self.roll + self.roll].copy_(rewards.squeeze())
+		self.masks[self.step*self.roll + self.roll].copy_(masks.squeeze())
 
-		self.actions[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(actions)
-		self.action_log_probs[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(action_log_probs)
-		self.value_preds[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(value_preds)
-		self.rewards[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(rewards)
-		self.masks[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(masks)
+
+		# self.actions[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(actions)
+		# self.action_log_probs[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(action_log_probs)
+		# self.value_preds[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(value_preds)
+		# self.rewards[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(rewards)
+		# self.masks[self.step*self.num_processes: (self.step+1)*self.num_processes].copy_(masks)
+		
 		self.step = (self.step + 1) % self.num_steps
+		self.roll = (self.roll+1)% self.num_rolls_per_sen
 
 	def insert_obs(self,obs):
-		self.returns = np.zeros((self.num_steps*self.num_processes+1,1))
+		# self.returns = np.zeros((self.num_steps*self.num_processes+1,1))
 		self.obs_s = torch.cat(obs[0])
 		self.obs_t = torch.cat(obs[1])
+
 
 	def after_update(self):
 		# self.obs[0].copy_(self.obs[-1])
@@ -83,8 +96,8 @@ class RolloutStorage(object):
 			# for step in reversed(range(self.rewards.size(0))):
 			# 	self.returns[step] = self.returns[step + 1] * \
 			# 		gamma * self.masks[step + 1] + self.rewards[step]
-
-			self.returns[-self.num_processes:] = next_value.data.cpu().numpy()
+			self.returns = np.zeros(((self.num_steps+1)*self.num_rolls_per_sen,self.num_processes,1))
+			self.returns[-1] = next_value.data.cpu().numpy()
 			for step in reversed(range(self.rewards.size(0))):
 				self.returns[step] = self.returns[step + 1] * \
 					gamma * self.masks[step + 1].data.cpu().numpy() + self.rewards[step].data.cpu().numpy()
@@ -104,18 +117,29 @@ class RolloutStorage(object):
 		obs_batch_s = self.obs_s[indices]
 		obs_batch_t = self.obs_t[indices]
 
-		actions_batch = self.actions[indices]
-		value_preds_batch = self.value_preds[indices]
-		return_batch = self.returns[indices]
-		old_action_log_probs_batch = self.action_log_probs[indices]
-		adv_targ = advantages[indices]
-		
-		actions_batch = self.actions.view(-1, self.actions.size(-1))[indices]
-		value_preds_batch = self.value_preds[:-1].view(-1, 1)[indices]
-		return_batch = self.returns[:-1].view(-1, 1)[indices]
-		old_action_log_probs_batch = self.action_log_probs.view(-1, 1)[indices]
-		adv_targ = advantages.view(-1, 1)[indices]
+		# actions_flat = _flatten_helper(self.actions)
+		# value_preds_flat = _flatten_helper(self.value_preds)
+		# returns_flat = _flatten_helper(self.returns)
+		# action_log_probs_flat = _flatten_helper(self.action_log_probs)
+		# advantages_flat = _flatten_helper(advantages)
 
+		actions_flat = self.actions.view(-1,1)		
+		value_preds_flat = self.value_preds.view(-1,1)
+		returns_flat = self.returns.view(-1,1)
+		action_log_probs_flat = self.action_log_probs.view(-1,1)
+		advantages_flat = advantages.view(-1,1)
+
+
+
+		actions_batch = actions_flat[indices]
+		value_preds_batch = value_preds_flat[indices]
+		return_batch = returns_flat[indices]
+		old_action_log_probs_batch = action_log_probs_flat[indices]
+		adv_targ = advantages_flat[indices]
+
+
+		
+		
 		return (obs_batch_s.cuda(),obs_batch_t.cuda()), actions_batch, \
 			value_preds_batch, return_batch,  old_action_log_probs_batch, adv_targ
 
