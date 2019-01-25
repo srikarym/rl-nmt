Fixed gradients issue

pytorch-a2c-ppo-acktr/a2c_ppo_acktr/model.py
@@ -37,18 +37,18 @@ class Policy(nn.Module):
 
 			self.base = base(obs_shape[0], **base_kwargs)
 
-		if action_space.__class__.__name__ == "Discrete":
-			num_outputs = action_space.n
-			self.dist = Categorical(self.base.output_size, num_outputs)
-		elif action_space.__class__.__name__ == "Box":
-			num_outputs = action_space.shape[0]
-			self.dist = DiagGaussian(self.base.output_size, num_outputs)
-		elif action_space.__class__.__name__ == "MultiBinary":
-			num_outputs = action_space.shape[0]
-			self.dist = Bernoulli(self.base.output_size, num_outputs)
-		else:
-			raise NotImplementedError
-		self.dist.to(device)
+		# if action_space.__class__.__name__ == "Discrete":
+		# 	num_outputs = action_space.n
+		# 	self.dist = Categorical(self.base.output_size, num_outputs)
+		# elif action_space.__class__.__name__ == "Box":
+		# 	num_outputs = action_space.shape[0]
+		# 	self.dist = DiagGaussian(self.base.output_size, num_outputs)
+		# elif action_space.__class__.__name__ == "MultiBinary":
+		# 	num_outputs = action_space.shape[0]
+		# 	self.dist = Bernoulli(self.base.output_size, num_outputs)
+		# else:
+		# 	raise NotImplementedError
+		# self.dist.to(device)
 
 	@property
 	def is_recurrent(self):
@@ -295,29 +295,30 @@ class AttnBase(NNBase):
 		# t = inputs[:,1,:].long().to(device)
 		s = inputs[0].long().to(device)
 		t = inputs[1].long().to(device) 
+		# s = inputs[0]
+		# t = inputs[1]
 
 		enc_out = self.encoder(s,(torch.ones([s.shape[0]])*s.shape[1]).to(device))
 
 		dec_hidden,dec_out = self.decoder(t,enc_out)
 
-		
-		outs = np.ones((dec_out.shape[0],dec_out.shape[-1]))
-		hidden = np.ones((dec_hidden.shape[0],dec_hidden.shape[-1]))
-
-
+		idx = []
 		for i in range(t.shape[0]):
 			l = (t[i] == self.pad_value).nonzero()
 			if l.shape[0] == 0:
 				l = -1
 			else:
 				l = l[0]-1
-			outs[i] = dec_out.data[i][l].data.cpu().numpy()
-			hidden[i] = dec_hidden.data[i][l].data.cpu().numpy()
+			idx.append(l)
 
-		outs = torch.tensor(outs).float().cuda()
-		hidden = torch.tensor(hidden).float().cuda()
+		outs = dec_out[np.arange(dec_out.shape[0]),idx,:]
+		hidden = dec_hidden[np.arange(dec_out.shape[0]),idx,:]
+
+
+
 		m = torch.nn.Softmax(dim = -1)
 		sm = m(outs)
+		b = sm.grad is None
 
 		# if self.is_recurrent:
 		# 	outs, rnn_hxs = self._forward_gru(outs, rnn_hxs, masks)

train_10_sen.py
@@ -16,8 +16,8 @@ from a2c_ppo_acktr.storage import RolloutStorage
 from arguments import get_args
 from utils import reshape_batch
 from tensorboardX import SummaryWriter
-import wandb
 
+import wandb
 args = get_args()
 if(args.use_wandb):
 	wandb.init(project=args.wandb_name)
@@ -29,12 +29,6 @@ if(args.use_wandb):
 # import wandb
 
 args = get_args()
-wandb.init(project=args.wandb_name)
-config = wandb.config
-
-config.batch_size = args.ppo_batch_size
-config.num_processes = args.num_processes
-config.lr = args.lr
 
 writer = SummaryWriter(args.log_dir)
 
@@ -64,7 +58,7 @@ envs = VecPyTorch(envs,'cuda')
 base_kwargs={'recurrent': False,'dummyenv':envs.dummyenv,'n_proc':args.num_processes}
 actor_critic = Policy(envs.observation_space.shape, envs.action_space,'Attn',base_kwargs)
 if(args.use_wandb):
-	wandb.watch(actor_critic)
+	wandb.watch(actor_critic.base.decoder)
 # wandb.watch(actor_critic)
 
 
@@ -138,7 +132,8 @@ for epoch in range(args.n_epochs+1):
 			rewards.append(np.mean(reward.squeeze(1).cpu().numpy()))
 
 		rollouts.insert_obs(reshape_batch(obs))
-		next_value = actor_critic.get_value(ob)
+		with torch.no_grad():
+			next_value = actor_critic.get_value(ob).detach()
 
 		rollouts.compute_returns(next_value, args.use_gae, args.gamma, args.tau)
 		end = time.time()